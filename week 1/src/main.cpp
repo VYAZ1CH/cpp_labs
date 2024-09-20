28311c681dadefc8df183a67e149921f75b127f9
Ilya <ila.ivannikov@mail.ru>
2024-09-20 23:58:49 +0300

    first commit

---
+++ week 1/src/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <cmath>
+#include <math.h>
+#include <iostream>
+using namespace std;
+double area(double a);
+int main()
+{
+    cout << area(10) << endl;
+}
+
+double area(double a) {
+
+   return (5.0 / 12.0) * (3 + sqrt(5)) * pow(a, 3);
+}
+
+
+
