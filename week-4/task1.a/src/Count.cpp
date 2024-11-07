05d1465857c022b53755a4aafa1d9f5b1181d9e3
VYAZ1CH <81022090+VYAZ1CH@users.noreply.github.com>
2024-11-07 20:01:42 +0300

    Update Count.cpp

--- week-4/task1.a/src/Count.cpp
+++ week-4/task1.a/src/Count.cpp
@@ -7,10 +7,9 @@ int countOccurrences(const std::string& str, const std::string& target) {
     int count = 0;
     std::string::size_type pos = 0;
 
-    // Èùåì âñå âõîæäåíèÿ target â str
     while ((pos = str.find(target, pos)) != std::string::npos) {
         ++count;
-        pos += target.length(); // Ïåðåõîäèì ê ñëåäóþùåìó ñèìâîëó ïîñëå íàéäåííîãî âõîæäåíèÿ
+        pos += target.length();
     }
 
     return count;
