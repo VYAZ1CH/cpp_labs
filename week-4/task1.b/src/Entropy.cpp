b154ba06c954d0da4f57430500b89f2c6fd98370
VYAZ1CH <81022090+VYAZ1CH@users.noreply.github.com>
2024-11-07 20:02:06 +0300

    Update Entropy.cpp

--- week-4/task1.b/src/Entropy.cpp
+++ week-4/task1.b/src/Entropy.cpp
@@ -7,20 +7,20 @@
 
 double calculateShannonEntropy(const std::string& input) {
     if (input.empty()) {
-        // Если строка пустая, возвращаем 0 return 0.0;
+        
     }
 
     std::unordered_map<char, int> frequency;
     int length = input.size();
 
-    // Подсчитываем частоту каждого символа
+ 
     for (char c : input) {
         frequency[c]++;
     }
 
     double entropy = 0.0;
 
-    // Вычисляем энтропию по формуле Шеннона
+    
     for (const auto& pair : frequency) {
         double probability = static_cast<double>(pair.second) / length;
         entropy -= probability * std::log2(probability);
